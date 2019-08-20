8cfb226e3b8f4c52728c1e211b218393713e8527
pengmingtao <1436276662@qq.com>
2019-08-20 18:43:18 +0800

    完成首页样式

---
+++ pages/index/index.js
@@ -0,0 +1,15 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    bannerList:[{pic:'../../sources/imgs/banner_01.png'},{pic:'../../sources/imgs/banner_01.png'}]
+  },
+  onLoad: function () {
+   
+  },
+  onShow:function () {
+
+  }
+})
